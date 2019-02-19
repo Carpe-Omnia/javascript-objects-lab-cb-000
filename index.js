8412a5b0a6a565bb8f4df5009fbd249d008bb6f8
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-19 18:42:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+  thing = object
+  thing[key] = value
+  return thing
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {}
 function deleteFromObjectByKey(object, key) {
